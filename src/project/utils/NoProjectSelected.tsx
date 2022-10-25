6fedccdcd539e761e8ee15b7eda5d3757f651b60
CicoDev <francescoloddo@gmail.com>
2022-10-26 00:04:52 +0200

    add brand fa

--- src/project/utils/NoProjectSelected.tsx
+++ src/project/utils/NoProjectSelected.tsx
@@ -4,7 +4,8 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- im
 const NoProjectSelected = () => {
     return (
         <div className="flex flex-col align-center p-5">
-            <FontAwesomeIcon className='text-5xl' icon={solid( 'cubes' )} />
+            <FontAwesomeIcon className="text-3xl" icon={solid( 'cubes' )} />
+            <h3 className="text-center text-lg mt-5">No data yet!</h3>
             <p className="text-center text-md mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur maximus imperdiet magna eget feugiat. Quisque elementum est tellus. Cras non dolor mauris. Donec pulvinar eleifend fringilla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
         </div>
     )
