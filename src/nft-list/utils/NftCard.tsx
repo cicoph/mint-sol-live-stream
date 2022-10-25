9b3de1f18364b56280684331532230b83c075c95
CicoCode <francescoloddo@gmail.com>
2022-10-25 13:03:22 +0200

    fix mintlivestream

--- src/nft-list/utils/NftCard.tsx
+++ src/nft-list/utils/NftCard.tsx
@@ -18,9 +18,12 @@ const NftCard: FunctionComponent<Props> = ({ nft }) => {
                 </a>
             </div>
             <div className="flex-1 min-w-0 text-xs">
-                <a rel="noreferrer" href={`https://explorer.solana.com/address/${nft.address}`} target="_blank" className="text-sm font-medium text-gray-900 truncate">
-                    {nft.name} - <Moment interval={1000} fromNow>{moment( nft.blockTime )}</Moment>
+                <a rel="noreferrer" href={`https://explorer.solana.com/address/${nft.address}`} target="_blank" className="font-medium text-gray-900 truncate">
+                    {nft.name}
                 </a>
+                <div className="font-medium text-gray-900 truncate">
+                    <Moment interval={1000} fromNow>{moment( nft.blockTime )}</Moment>
+                </div>
             </div>
         </div>
     </div>
