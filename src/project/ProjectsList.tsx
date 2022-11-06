afcda886dae200343ac7f8d48b5aeeeb7101c3e2
CicoDev <francescoloddo@gmail.com>
2022-11-06 14:19:14 +0100

    change logic sort

--- src/project/ProjectsList.tsx
+++ src/project/ProjectsList.tsx
@@ -7,13 +7,14 @@ import Moment from 'react-moment';
 import Project  from './utils/Project';
 import ProjectCard  from './utils/ProjectCard';
 import ButtonsTime from './utils/ButtonsTime';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 // import ProjectDetails from './ProjectDetails';
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 export type onSelectProject = ( arg0: Project ) => any;
 
-interface Props {
+type Props = {
     projectSelected: onSelectProject;
 }
 
@@ -21,40 +22,42 @@ const ProjectsList : FunctionComponent<Props> = ( { projectSelected } ) => {
     const cachedCallback = useRef<NodeJS.Timer | null>(null);
 
     const [ timeFrame, setTimeFrame ] = useState<number>(5);
-    const [ sort, setSort ] = useState<boolean>( true )
     const [ projects, setProjects ] = useState<Project[]>([]);
     const [ lastUpdate, setLastUpdate ] = useState<Date>( new Date )
 
-    const sortDescByMintNumber = ( a: Project, b: Project ) => sort ? b.nfts?.length - a.nfts?.length : a.nfts?.length - b.nfts?.length
-
-    const getProjectsWithFetch = async ( timeFrame: number, sort: boolean ): Promise<void> => {
-        const URL = `${SERVER_URL}/projects/${timeFrame}`
-        fetch( URL )
-        .then( async (response) => await response.json())
-        .then( ( result: Project[] ) => setProjects( result.sort( sortDescByMintNumber ) ) )
-        .catch((error) => {
-            console.log(error)
-        });
-    };
+    const [ sort, setSort ] = useState<boolean>( true )
+    const [ iconSort, setIconSort ] = useState<IconDefinition>( solid('arrow-down-wide-short' ) )
 
     const handlerProjectDetails = async ( id: string ) => {
         await fetch( `${SERVER_URL}/project/id/${id}` )
         .then( async (response) => await response.json())
         .then( ( result: Project ) => projectSelected( result ) )
-        .catch((error) => {
-            console.log(error)
-        });
+        .catch( error => console.log(error) )
     }
 
-    useEffect( () => setLastUpdate(new Date), [projects] )
+    const sortDescByMintNumber = ( a: Project, b: Project ) => sort ? b.nfts?.length - a.nfts?.length : a.nfts?.length - b.nfts?.length
+
+    async function getProjectsWithFetch ( timeFrame: number ): Promise<void> {
+        fetch( `${SERVER_URL}/projects/${timeFrame}` )
+        .then( async response => await response.json())
+        .then( ( result: Project[] ) => setProjects( result.sort( sortDescByMintNumber ) ) )
+        .catch( error =>  console.log(error) )
+    };
+
+    useEffect( () => setLastUpdate(new Date), [ projects ] )
 
     useEffect( () => {
-        getProjectsWithFetch( timeFrame, sort )
-        cachedCallback.current = setInterval( () => getProjectsWithFetch( timeFrame, sort ), 5 * 60 * 1000 )
+        getProjectsWithFetch( timeFrame )
+        cachedCallback.current = setInterval( () => getProjectsWithFetch( timeFrame ), 5 * 60 * 1000 )
         return () => {
             if( cachedCallback.current ) clearInterval( cachedCallback.current )
         }
-    }, [ timeFrame, sort ]);
+    }, [ timeFrame ]);
+
+    useEffect( () => {
+        projects.sort( sortDescByMintNumber )
+        sort ? setIconSort( solid('arrow-down-wide-short') ) : setIconSort( solid('arrow-up-short-wide' ) )
+    }, [ sort ])
     
     return (
         <section>
@@ -71,16 +74,15 @@ const ProjectsList : FunctionComponent<Props> = ( { projectSelected } ) => {
                     <div className="flex -mx-6 border-y p-2 justify-between items-center">
                         <div className="inline-flex" role="group">
                             <button 
-                                onClick={() => getProjectsWithFetch( timeFrame, sort )}
+                                onClick={() => getProjectsWithFetch( timeFrame )}
                                 className="py-2 px-4 shadow-sm text-xs font-medium text-gray-900 bg-white rounded-sm hover:bg-gray-100">
                                 {/* <FontAwesomeIcon icon={icon({name: 'rotate-right', style: 'solid'})} /> */}
                                 <FontAwesomeIcon icon={solid('arrow-rotate-right')} />
                             </button>
                             <button 
-                                onClick={ () => setSort( (prevSort) => !prevSort )}
+                                onClick={ () => setSort( prevSort => !prevSort ) }
                                 className="py-2 px-4 shadow-sm text-xs font-medium text-gray-900 bg-white rounded-sm hover:bg-gray-100">
-                                { sort && <FontAwesomeIcon icon={solid('arrow-down-wide-short')} /> }
-                                { !sort && <FontAwesomeIcon icon={solid('arrow-up-short-wide')} /> }
+                                <FontAwesomeIcon icon={ iconSort } />
                             </button>
                         </div>
                         <ButtonsTime onChosen={ (time: number) => setTimeFrame( time )} />
